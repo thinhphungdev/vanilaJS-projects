bb193131d67e236218639b37366eba7218079277
howardphungdev <phungthinh0946519356@gmail.com>
2023-10-30 15:51:37 +0700

    feat: get meal to the DOM fn

--- meal-finder/script.js
+++ meal-finder/script.js
@@ -3,7 +3,10 @@ const form = document.getElementById('submit');
 const mealsEl = document.getElementById('meals');
 const resultHeading = document.getElementById('result-heading');
 const singleMealEl = document.getElementById('single-meal');
+////////////////////////////////////////
+const mealDBURL = 'https://www.themealdb.com/api/json/v1/1/';
 
+////////////////////////////////////////
 // HELPER METHODS
 //service
 function searchMeal(e) {
@@ -17,21 +20,53 @@ function searchMeal(e) {
 
   if (!term) alert('Please enter a search term');
 
-  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+  fetch(`${mealDBURL}/search.php?s=${term}`)
     .then((res) => res.json())
     .then((data) => renderMeals(data, term));
 }
 
 function getMealById(mealId) {
-  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+  fetch(`${mealDBURL}/lookup.php?i=${mealId}`)
     .then((res) => res.json())
     .then((data) => {
       const meal = data.meals[0];
 
+      console.log(meal);
       addMealToDOM(meal);
     });
 }
 
+function addMealToDOM(meal) {
+  const ingredients = [];
+
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    }
+  }
+
+  singleMealEl.innerHTML = `
+    <div class="single-meal">
+      <h1>${meal.strMeal}</h1>
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+      <div class="single-meal-info">
+        ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+        ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+      </div>
+      <div class="main">
+        <p>${meal.strInstructions}</p>
+        <h2>Ingredients</h2>
+        <ul>
+          ${ingredients.map((ing) => `<li>${ing}</li>`).join('')}
+        </ul>
+      </div>
+    </div>
+  `;
+}
+
+////////////////////////////////////////
 // render
 function renderMeals(data, term) {
   if (data.meals === null) {
@@ -56,10 +91,11 @@ function renderMeals(data, term) {
   search.value = '';
 }
 
+////////////////////////////////////////
 // Event listener
 form.addEventListener('submit', searchMeal);
 
-// Click on each Meal Item to show full meal details
+// Click on each Meal Item to show full detail about the meal
 mealsEl.addEventListener('click', (e) => {
   e.stopPropagation();
 
@@ -68,5 +104,5 @@ mealsEl.addEventListener('click', (e) => {
   if (!targetMealEl.classList.contains('meal-info')) return false;
 
   const mealID = targetMealEl.getAttribute('data-mealid');
-  console.log(mealID);
+  getMealById(mealID);
 });
