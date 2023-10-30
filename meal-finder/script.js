4729424b099f7de56a5ca47755896842fa82ac30
howardphungdev <phungthinh0946519356@gmail.com>
2023-10-30 10:01:53 +0700

    feat: dom selection

---
+++ meal-finder/script.js
@@ -0,0 +1,7 @@
+const search = document.getElementById('search');
+const form = document.getElementById('submit');
+const mealsEl = document.getElementById('meals');
+const resultText = document.getElementById('result-heading');
+const singleMealEl = document.getElementById('single-meal');
+
+// Event listener
