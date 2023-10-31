b03210f3a0238dcb5f7f8d1fa95d3c42445ffbde
howardphungdev <phungthinh0946519356@gmail.com>
2023-10-31 10:11:07 +0700

    feat: render post to the dom

--- infinite-scroll/script.js
+++ infinite-scroll/script.js
@@ -5,8 +5,11 @@ const filter = document.getElementById('filter');
 const limit = 5;
 const page = 1;
 
+// INIT
+showPosts();
+
 // SERVICE
-async function fetchPost() {
+async function getPost() {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
   );
@@ -14,4 +17,41 @@ async function fetchPost() {
   const data = await res.json();
 
   return data;
+}
+
+// DOM related function
+async function showPosts() {
+  const posts = await getPost();
+
+  postContainer.innerHTML = '';
+
+  posts.forEach((post) => {
+    const postElHTML = `
+      <div class="post">
+        <div class="number">${post.id}</div>
+        <div class="post-info">
+          <h2 class="post-title">${post.title}</h2>
+          <p class="post-body">${post.body}</p>
+        </div>
+    </div>
+    `;
+
+    postContainer.insertAdjacentHTML('beforeend', postElHTML);
+  });
+
+  // ALTERNATIVE
+
+  //   posts.forEach((post) => {
+  //     const postEl = document.createElement('div');
+  //     postEl.classList.add('post');
+  //     postEl.innerHTML = `
+  //     <div class="number">${post.id}</div>
+  //     <div class="post-info">
+  //       <h2 class="post-title">${post.title}</h2>
+  //       <p class="post-body">${post.body}</p>
+  //     </div>
+  //     `;
+
+  //     postContainer.appendChild(postEl);
+  //   });
 }
