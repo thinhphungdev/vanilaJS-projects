2f5b6e4d62e93a65728650069c099506017b1243
howardphungdev <phungthinh0946519356@gmail.com>
2023-11-11 08:09:37 +0700

    feat: creat list & render list & scramble list of items

--- sortable-table/script.js
+++ sortable-table/script.js
@@ -13,3 +13,42 @@ const richestPeople = [
   'Michael Bloomberg',
   'Larry Page',
 ];
+
+// Store list items
+const listItems = [];
+
+// const dragStartIndex;
+
+createList();
+
+// Create and Insert List Item to the DOM
+function createList() {
+  const scrambledList = scrambleList(richestPeople);
+
+  scrambledList.map((person, index) => {
+    const listItem = document.createElement('li');
+    listItem.setAttribute('data-index', index);
+    renderList(listItem, person, index);
+  });
+}
+
+function scrambleList(list) {
+  return list
+    .map((value) => ({ value: value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((obj) => obj.value);
+}
+
+function renderList(listItem, person, index) {
+  listItem.innerHTML = `
+    <span class="number">${index + 1}</span>
+    <div class="draggable" draggable="true">
+        <p class="person-name">${person}</p>
+        <i class="fas fa-grip-lines">></i>
+    </div>
+`;
+
+  listItems.push(listItem);
+
+  draggableList.appendChild(listItem);
+}
