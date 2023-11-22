fd979b2259c06fa1d6460a8ee17aeadc514c849a
howardphungdev <phungthinh0946519356@gmail.com>
2023-11-22 10:59:26 +0700

    refactor

--- drag-and-drop-board/todo.js
+++ drag-and-drop-board/todo.js
@@ -5,15 +5,35 @@ const todoLane = document.getElementById('todo-lane');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const taskContent = input.value;
-  if (!taskContent) return;
 
-  const task = document.createElement('p');
-  task.classList.add('task');
-  task.setAttribute('draggable', true);
-  task.textContent = taskContent;
+  const taskContent = input.value;
+
+  const newTask = createNewTask(taskContent);
+
+  addDragEventHandler(newTask);
+
+  todoLane.appendChild(newTask);
 
   input.value = '';
-
-  todoLane.appendChild(task);
 });
+
+function createNewTask(taskContent) {
+  if (!taskContent) return;
+
+  const newTask = document.createElement('p');
+  newTask.classList.add('task');
+  newTask.setAttribute('draggable', true);
+  newTask.textContent = taskContent;
+
+  return newTask;
+}
+
+function addDragEventHandler(element) {
+  element.addEventListener('dragstart', () => {
+    element.classList.add('is-dragging');
+  });
+
+  element.addEventListener('dragend', () => {
+    element.classList.remove('is-dragging');
+  });
+}
