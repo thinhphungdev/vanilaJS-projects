f9064f90aa473e0d9d3d534fd099a0c12110903f
howardphungdev <phungthinh0946519356@gmail.com>
2023-11-24 15:16:42 +0700

    finish drag and drop board

--- drag-and-drop-board/drag.js
+++ drag-and-drop-board/drag.js
@@ -14,5 +14,34 @@ draggables.forEach((task) => {
 dropables.forEach((zone) => {
   zone.addEventListener('dragover', (e) => {
     e.preventDefault();
+
+    const bottomTask = insertAboveTask(zone, e.clientY);
+    const currentTask = document.querySelector('.is-dragging');
+
+    if (bottomTask) {
+      zone.insertBefore(currentTask, bottomTask);
+    } else {
+      zone.appendChild(currentTask);
+    }
   });
 });
+
+const insertAboveTask = (zone, mouseY) => {
+  const notDraggingTasks = zone.querySelectorAll('.task:not(.is-dragging)');
+
+  let closestTask = null;
+  let closestOffset = Number.NEGATIVE_INFINITY;
+
+  notDraggingTasks.forEach((task) => {
+    const { top } = task.getBoundingClientRect();
+
+    const offset = mouseY - top;
+
+    if (offset < 0 && offset > closestOffset) {
+      closestOffset = offset;
+      closestTask = task;
+    }
+  });
+
+  return closestTask;
+};
