02866e5ddd70524956bda3c07be2084f56d63113
howardphungdev <phungthinh0946519356@gmail.com>
2023-11-28 13:59:57 +0700

    feat: add to cart and remove from cart

--- coffeemasters-vanilla/services/Order.js
+++ coffeemasters-vanilla/services/Order.js
@@ -9,8 +9,21 @@ export async function addToCart(id) {
 
   const productAlreadyInCart = results.length === 1;
 
-  if (!productAlreadyInCart) {
-    app.store.cart;
-    return;
+  if (productAlreadyInCart) {
+    // Update the current Item
+    app.store.cart = app.store.cart.map((product) => {
+      return product.product.id === id
+        ? { ...product, quantity: product.quantity + 1 }
+        : product;
+    });
+  } else {
+    // add new product
+    app.store.cart = [...app.store.cart, { product, quantity: 1 }];
   }
+}
+
+export function removeFromCart(id) {
+  app.store.cart = app.store.cart.filter(
+    (product) => product.product.id !== id
+  );
 }
