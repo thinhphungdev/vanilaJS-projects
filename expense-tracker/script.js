c3305354b6b5d5195e4c6da3ee8e6587dd3c4166
thinhphungdev <phungthinh0946519356@gmail.com>
2024-01-26 08:22:10 +0700

    show initial list of dummy transactions

--- expense-tracker/script.js
+++ expense-tracker/script.js
@@ -5,3 +5,50 @@ const list = document.getElementById('list');
 const form = document.getElementById('form');
 const text = document.getElementById('text');
 const amount = document.getElementById('amount');
+
+const dummyTransactions = [
+  { id: 1, text: 'Salary', amount: 300 },
+  { id: 2, text: 'Book', amount: -30 },
+  { id: 3, text: 'Camera', amount: -150 },
+];
+
+let transactions = dummyTransactions;
+
+// Add transaction to DOM list
+function addTransactionDOM(transaction) {
+  // get sign
+  const sign = transaction.amount < 0 ? '-' : '+';
+
+  const item = document.createElement('li');
+  // Add class CSS based on value
+  item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+
+  item.innerHTML = `
+    ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span>
+    <button class="delete-btn">X</button>
+  `;
+
+  list.appendChild(item);
+}
+
+function addTransactionDOM2(transaction) {
+  const sign = transaction.amount < 0 ? '-' : '+';
+
+  const transactionItemHtml = `
+    <li class=${transaction.amount < 0 ? 'minus' : 'plus'}>
+    ${transaction.text} <span>${sign}${Math.abs(
+    transaction.amount
+  )}</span><button class="delete-btn">x</button>
+    </li>
+    `;
+
+  list.insertAdjacentHTML('beforeend', transactionItemHtml);
+}
+
+// Init App
+function init() {
+  list.innerHTML = '';
+  transactions.forEach((transaction) => addTransactionDOM2(transaction));
+}
+
+init();
